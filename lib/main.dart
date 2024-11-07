2d16ba6de6364857a4ff314aeb0c7dd9e7834d9e
areejalhamed <areejalhamed2002@gmail.com>
2024-11-07 10:51:35 +0300

    login

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:project/view/screen/home_page/home_page.dart';
+import 'package:project/view/screen/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      debugShowCheckedModeBanner: false,
+      home: Loginpage(),
+    );
+  }
+}
